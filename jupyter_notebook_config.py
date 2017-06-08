fe824f46401745d291e1904207d9d8d914fee6c1
Caleb Chan <Calebium@gmail.com>
2017-06-08 13:36:40 -0700

    Add code for post_save_hook() to save a .py version of the current Jupyter notebook whenever it is saved

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -18,3 +18,60 @@ c.NotebookApp.open_browser = False
 
 ## Whether to allow the user to run the notebook as root.
 c.NotebookApp.allow_root = True
+
+#------------------------------------------------------------------------------
+# FileContentsManager(FileManagerMixin,ContentsManager) configuration
+#------------------------------------------------------------------------------
+
+## Python callable or importstring thereof
+#
+#  to be called on the path of a file just saved.
+#
+#  This can be used to process the file on disk, such as converting the notebook
+#  to a script or HTML via nbconvert.
+#
+#  It will be called as (all arguments passed by keyword)::
+#
+#      hook(os_path=os_path, model=model, contents_manager=instance)
+#
+#  - path: the filesystem path to the file just written - model: the model
+#  representing the file - contents_manager: this ContentsManager instance
+
+# The function script_post_save() is used for saving a Python script (for version
+# control wth Git) whenever a Jupyter notebook is saved, and is lifted from:
+#
+#   https://github.com/jupyter/notebook/blob/master/docs/source/extending/savehooks.rst
+
+import io
+import os
+from notebook.utils import to_api_path
+
+_script_exporter = None
+
+def script_post_save(model, os_path, contents_manager, **kwargs):
+    """convert notebooks to Python script after save with nbconvert
+
+    replaces `ipython notebook --script`
+    """
+    from nbconvert.exporters.script import ScriptExporter
+
+    if model['type'] != 'notebook':
+        return
+
+    global _script_exporter
+
+    if _script_exporter is None:
+        _script_exporter = ScriptExporter(parent=contents_manager)
+
+    log = contents_manager.log
+
+    base, ext = os.path.splitext(os_path)
+    py_fname = base + '.py'
+    script, resources = _script_exporter.from_filename(os_path)
+    script_fname = base + resources.get('output_extension', '.txt')
+    log.info("Saving script /%s", to_api_path(script_fname, contents_manager.root_dir))
+
+    with io.open(script_fname, 'w', encoding='utf-8') as f:
+        f.write(script)
+
+c.FileContentsManager.post_save_hook = script_post_save
