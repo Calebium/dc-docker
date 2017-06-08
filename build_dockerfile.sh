3ece1f1d3a61fd785d3f2500450e89b254488f10
Caleb Chan <Calebium@gmail.com>
2017-06-08 13:46:19 -0700

    Create a new Docker user based on the host user that is used to build and run the Docker container. This solves file permission problems where files created/modified from within the container in shared volumes are owned by root, which are inaccessible to the host user

--- build_dockerfile.sh
+++ build_dockerfile.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
 # Run this shell script to build the docker image for DeepCell
 
-docker build -t calebium/dc-docker -f Dockerfile.DeepCell .
+docker build -t calebium/dc-docker -f Dockerfile.DeepCell \
+       --build-arg USERNAME=$(id -un) --build-arg GROUPNAME=$(id -gn) --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
+       .
